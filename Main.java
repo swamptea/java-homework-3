749c751609665eedb6ef9cf60b3c463bde6a2c6d
swamptea <96385599+swamptea@users.noreply.github.com>
2022-06-06 11:56:45 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,72 +5,50 @@ import java.util.List;
 
 public class Main {
     static StringBuilder log = new StringBuilder();
+    static String done = "успешно\n";
+    static String error = "при создании произошла ошибка\n";
+
+    static void createFile(File file, boolean dir) throws IOException {
+        String type = (dir ? "директории " : "файла ");
+        log.append(new Date() + " создание " + type + file.getName() + "... ");
+        if (dir ? file.mkdir() : file.createNewFile())
+            log.append(done);
+        else log.append(error);
+    }
 
     public static void main(String[] args) {
 
-        File src = new File("/home/alexandra/Games/src");
-        File res = new File("/home/alexandra/Games/res");
-        File savegames = new File("/home/alexandra/Games/savegames");
-        File temp = new File("/home/alexandra/Games/temp");
-        File main = new File("/home/alexandra/Games/src/main");
-        File test = new File("/home/alexandra/Games/src/test");
-        File drawables = new File("/home/alexandra/Games/res/drawables");
-        File vectors = new File("/home/alexandra/Games/res/vectors");
-        File icons = new File("/home/alexandra/Games/res/icons");
-        File mainJava = new File("/home/alexandra/Games/src/main/Main.java");
-        File utilsJava = new File("/home/alexandra/Games/src/main/Utils.java");
-        File tempTxt = new File("/home/alexandra/Games/temp/temp.txt");
-        log.append(new Date() + " создание директории src... ");
-        if (src.mkdir())
-            log.append("успешно\n");
-        else log.append("при создании произошла ошибка\n");
-        log.append(new Date() + " создание директории res... ");
-        if (res.mkdir())
-            log.append("успешно\n");
-        else log.append("при создании произошла ошибка\n");
-        log.append(new Date() + " создание директории savegames... ");
-        if (savegames.mkdir())
-            log.append("успешно\n");
-        else log.append("при создании произошла ошибка\n");
-        log.append(new Date() + " создание директории temp... ");
-        if (temp.mkdir())
-            log.append("успешно\n");
-        else log.append("при создании произошла ошибка\n");
-        log.append(new Date() + " создание директории main... ");
-        if (main.mkdir())
-            log.append("успешно\n");
-        else log.append("при создании произошла ошибка\n");
-        log.append(new Date() + " создание директории test... ");
-        if (test.mkdir())
-            log.append("успешно\n");
-        else log.append("при создании произошла ошибка\n");
-        log.append(new Date() + " создание директории drawables... ");
-        if (drawables.mkdir())
-            log.append("успешно\n");
-        else log.append("при создании произошла ошибка\n");
-        log.append(new Date() + " создание директории vectors... ");
-        if (vectors.mkdir())
-            log.append("успешно\n");
-        else log.append("при создании произошла ошибка\n");
-        log.append(new Date() + " создание директории icons... ");
-        if (icons.mkdir())
-            log.append("успешно\n");
-        else log.append("при создании произошла ошибка\n");
-        try {
-            log.append(new Date() + " создание файла Main.java... ");
-            if (mainJava.createNewFile())
-                log.append("успешно\n");
-            else log.append("при создании произошла ошибка\n");
-            log.append(new Date() + " создание файла Utils.java... ");
-            if (utilsJava.createNewFile())
-                log.append("успешно\n");
-            else log.append("при создании произошла ошибка\n");
-            log.append(new Date() + " создание файла temp.txt... ");
-            if (tempTxt.createNewFile())
-                log.append("успешно\n");
-            else log.append("при создании произошла ошибка\n");
-        } catch (IOException e) {
-            e.printStackTrace();
+        File src, res, savegames, temp, main, test, drawables, vectors, icons,
+                mainJava, utilsJava, tempTxt;
+
+        List<File> dirs = new ArrayList<>();
+        List<File> files = new ArrayList<>();
+        dirs.add(src = new File("/home/alexandra/Games/src"));
+        dirs.add(res = new File("/home/alexandra/Games/res"));
+        dirs.add(savegames = new File("/home/alexandra/Games/savegames"));
+        dirs.add(temp = new File("/home/alexandra/Games/temp"));
+        dirs.add(main = new File("/home/alexandra/Games/src/main"));
+        dirs.add(test = new File("/home/alexandra/Games/src/test"));
+        dirs.add(drawables = new File("/home/alexandra/Games/res/drawables"));
+        dirs.add(vectors = new File("/home/alexandra/Games/res/vectors"));
+        dirs.add(icons = new File("/home/alexandra/Games/res/icons"));
+        files.add(mainJava = new File("/home/alexandra/Games/src/main/Main.java"));
+        files.add(utilsJava = new File("/home/alexandra/Games/src/main/Utils.java"));
+        files.add(tempTxt = new File("/home/alexandra/Games/temp/temp.txt"));
+
+        for (File dir : dirs) {
+            try {
+                createFile(dir, true);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        for (File file : files) {
+            try {
+                createFile(file, false);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
 
         try (FileWriter writer = new FileWriter(tempTxt)) {
